72ec3f3c7d1ecff1d228d4f4fdf46463358e8028
Moritz Wright <me@moritzwright.com>
2021-01-08 14:01:19 +0100

    activate demo form

--- server.js
+++ server.js
@@ -35,51 +35,51 @@ app.get("/", (req, res) => {
 app.post("/send", (req, res) => {
   console.log(req.body);
 
-  //   const output = `
-  //     <p>You have a new contact request</p>
-  //     <h3>Contact Details</h3>
-  //     <ul>
-  //         <li>Name: ${req.body.name}</li>
-  //         <li>Company: ${req.body.company}</li>
-  //         <li>Email: ${req.body.email}</li>
-  //         <li>Phone: ${req.body.phone}</li>
-  //     </ul>
-  //     <h3>Message</h3>
-  //     <p>${req.body.message}</p>
-  //   `;
+  const output = `
+      <p>You have a new contact request</p>
+      <h3>Contact Details</h3>
+      <ul>
+          <li>Name: ${req.body.name}</li>
+          <li>Company: ${req.body.company}</li>
+          <li>Email: ${req.body.email}</li>
+          <li>Phone: ${req.body.phone}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
 
-  //   // async..await is not allowed in global scope, must use a wrapper
-  //   async function main() {
-  //     // create reusable transporter object using the default SMTP transport
-  //     let transporter = nodemailer.createTransport({
-  //       host: "smtp.strato.de",
-  //       port: 587,
-  //       secure: false, // true for 465, false for other ports
-  //       auth: {
-  //         user: "test@hemdundfliege.com", // generated ethereal user
-  //         pass: "!91MaWHh19!", // generated ethereal password
-  //       },
-  //     });
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.strato.de",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "test@hemdundfliege.com", // generated ethereal user
+        pass: "!91MaWHh19!", // generated ethereal password
+      },
+    });
 
-  //     // send mail with defined transport object
-  //     let info = await transporter.sendMail({
-  //       from: '"Moritz Wright" <test@hemdundfliege.com>', // sender address
-  //       to: req.body.email, // list of receivers
-  //       subject: "Hello ✔", // Subject line
-  //       text: "Hello world?", // plain text body
-  //       html: output, // html body
-  //     });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Moritz Wright" <test@hemdundfliege.com>', // sender address
+      to: req.body.email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: output, // html body
+    });
 
-  //     console.log("Message sent: %s", info.messageId);
-  //     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  //     // Preview only available when sending through an Ethereal account
-  //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  //     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  //     res.render("main", { msg: "Email has been sent" });
-  //   }
-  //   main().catch(console.error);
+    res.render("main", { msg: "Email has been sent" });
+  }
+  main().catch(console.error);
 });
 
 app.listen(8080, () => log(`Server is connected on port 8080`));
